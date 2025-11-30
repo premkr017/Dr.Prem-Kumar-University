71eea3e4f3028927d2f78d03a9540f19ed99ab5e
Prem Kumar <premkr017@gmail.com>
2025-11-30 21:11:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dr.Prem Kumar University</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Arial, sans-serif;
+        }
+
+        div.wrapper,
+        .wrapper100 {
+            display: block;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            text-align: left;
+            word-wrap: break-word;
+        }
+img {
+    overflow-clip-margin: content-box;
+    overflow: clip;
+}
+a {
+    color: #337ab7;
+    text-decoration: none;
+}
+#header #hgroup h1 {
+    font-size: 2em;
+}
+        #header {
+            padding: 10px 0;
+        }
+
+        @media screen and (min-width: 1200px) {
+
+            #header,
+            #topnav,
+            #intro,
+            #container,
+            #footer,
+            #copyright,
+            .stretched {
+                max-width: 1200px;
+                margin: 0 auto;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header id="header" class="full_width clear">
+            <div id="hgroup">
+                <h1>
+                    <a href="index.php">
+                        <img src="dr.prem logo.png" alt="Dr.Prem Kumar University Logo">
+                    </a>
+
+                </h1>
+            </div>
+            <div id="header-contact">
+                <h1 align="center">Welcome to Dr.Prem Kumar University Portal </h1>
+            </div>
+        </header>
+    </div>
+</body>
+
+</html>
